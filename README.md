9b6ecc0fc74dadb03f6c48cb8d4cc414c391e4a0
Jay Won <jason@sudokrew.com>
2015-03-30 01:05:03 -1000

    Added ReactiveVar example, momentjs and added counter and some comments.

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ See following links for recommended(best practices) project structure:
 
 [Building Modular Apps in Meteor]
 (http://tech.exponential.io/meteor/building-large-modular-apps-meteor/)
+
+For this project you will need to install:
+
+```
+meteor add reactive-var
+meteor add momentjs:moment
+
+```
--- client/views/home.js
+++ client/views/home.js
@@ -1,23 +1,36 @@
 //client/views/home.js
 
 Template.posts.created = function(){
-
+  //set up template instance variables
+  this.timer = new ReactiveVar(moment().format('MMMM Do YYYY, h:mm:ss a'));
 };
 
 Template.posts.rendered = function(){
-
+  var self = this;
+  //any processing that needs to happen on load
+  var interval = setInterval(function(){
+    self.timer.set(moment().format('MMMM Do YYYY, h:mm:ss a')); 
+  }, 1000);
 };
 
 Template.posts.destroyed = function(){
-
+  //any cleanup that needs to happen
 };
 
+//define helper methods accessible from your template
 Template.posts.helpers({
+  //this is a reactive data source
   postings: function(){
     return Posts.find({}, {sort: {score: -1, name: 1}});
+  },
+  //this is also a reactive data source
+  timer: function(){
+    return Template.instance().timer.get();
   }
+
 });
 
+//bind any event handlers to your template
 Template.post.events({
   'click a.upvote': function(event, template) {
     event.preventDefault();
